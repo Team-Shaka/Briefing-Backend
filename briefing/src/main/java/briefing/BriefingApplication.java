cae6c745154f381772e3ed5cd9decbd67e7cbf45
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-08-23 13:37:11 +0900

    feat: 생성 및 수정일시 공통 도메인 클래스 구현

--- briefing/src/main/java/briefing/BriefingApplication.java
+++ briefing/src/main/java/briefing/BriefingApplication.java
@@ -2,8 +2,10 @@ package briefing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BriefingApplication {
 
   public static void main(String[] args) {
