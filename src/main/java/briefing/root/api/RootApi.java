fef107ed2d7cf31b9eaeac4f8c1469efb0149173
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-09-14 17:26:19 +0900

    Feature/1 👷 [CI] ci/cd 파이프라인 구축 필요한 파일 추가 (#8)
    * :construction_worker: Ci : nginx 설정 추가
    
    * :construction_worker: Ci : ci/cd 파이프라인 필요한 파일 추가

---
+++ src/main/java/briefing/root/api/RootApi.java
@@ -0,0 +1,18 @@
+package briefing.root.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+
+public class RootApi {
+
+    @GetMapping("/")
+    public String healthCheck(){
+        return "I'm healthy!";
+    }
+}
