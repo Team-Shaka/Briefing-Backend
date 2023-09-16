3bdd73c42165b104eeeba41280b030e81681af7c
CYY1007 <younguk86@gmail.com>
2023-09-17 02:18:52 +0900

    :sparkles: Feat : spring security 설정 추가

---
+++ src/main/java/briefing/security/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,47 @@
+package briefing.security.handler;
+
+import briefing.exception.ErrorCode;
+import briefing.exception.common.ApiErrorResult;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(JwtAccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/json; charset=UTF-8");
+        response.setStatus(403);
+        PrintWriter writer = response.getWriter();
+
+        ApiErrorResult apiErrorResult = ApiErrorResult.builder()
+                .isSuccess(false)
+                .code(ErrorCode._FORBIDDEN.getCode())
+                .message(ErrorCode._FORBIDDEN.getMessage())
+                .result(null)
+                .build();
+        try{
+            writer.write(apiErrorResult.toString());
+        }catch(NullPointerException e){
+            LOGGER.error("응답 메시지 작성 에러", e);
+        }finally{
+            if(writer != null) {
+                writer.flush();
+                writer.close();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/briefing/security/handler/JwtAuthenticationExceptionHandler.java
@@ -0,0 +1,43 @@
+package briefing.security.handler;
+
+import briefing.exception.ErrorCode;
+import briefing.exception.common.ApiErrorResult;
+import briefing.exception.handler.JwtAuthenticationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class JwtAuthenticationExceptionHandler extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request,response);
+        }catch (JwtAuthenticationException authException){
+            response.setContentType("application/json; charset=UTF-8");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+            PrintWriter writer = response.getWriter();
+            String errorCodeName = authException.getMessage();
+            ErrorCode code = ErrorCode.valueOf(errorCodeName);
+
+            ApiErrorResult apiErrorResult = ApiErrorResult.builder()
+                    .isSuccess(false)
+                    .code(code.getCode())
+                    .message(code.getMessage())
+                    .result(null)
+                    .build();
+
+            writer.write(apiErrorResult.toString());
+            writer.flush();
+            writer.close();
+        }
+    }
+}
