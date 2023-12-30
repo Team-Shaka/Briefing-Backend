b3cbaf7c34e893d7d67c851f119481f7ec873490
CYY1007 <younguk86@gmail.com>
2023-12-30 21:53:51 +0900

    :recycle: Refactor : Swagger 로그인 및 리터칭

--- src/main/java/briefing/BriefingApplication.java
+++ src/main/java/briefing/BriefingApplication.java
@@ -1,11 +1,18 @@
 package briefing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "local server"),
+        @Server(url = "https://dev.briefing.store", description = "dev server"),
+        @Server(url = "https://api.newsbreifing.store", description = "release server")
+})
 
 @SpringBootApplication
 @EnableFeignClients
