246ab82edaf00fc960643e058398411a8434ef65
swa07016 <swa07016@naver.com>
2024-01-10 15:47:06 +0900

    :recycle: Refactor: Chatting & Message 관련 코드 제거

--- src/main/java/briefing/chatting/domain/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package briefing.chatting.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import briefing.chatting.domain.Message;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {}
