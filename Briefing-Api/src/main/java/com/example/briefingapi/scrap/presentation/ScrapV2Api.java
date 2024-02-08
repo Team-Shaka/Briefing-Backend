fac98086fa1255220319425bd3e7a54a8d472b48
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2024-02-08 11:54:44 +0900

    :memo: Docs : 인가 처리가 필요한 API 대상 토큰 관련 명세 진행 (#179)

--- Briefing-Api/src/main/java/com/example/briefingapi/scrap/presentation/ScrapV2Api.java
+++ Briefing-Api/src/main/java/com/example/briefingapi/scrap/presentation/ScrapV2Api.java
@@ -5,6 +5,10 @@ import com.example.briefingapi.scrap.business.ScrapV2Facade;
 import com.example.briefingapi.scrap.presentation.dto.ScrapRequest;
 import com.example.briefingapi.scrap.presentation.dto.ScrapResponse;
 import com.example.briefingcommon.common.presentation.response.CommonResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.briefingapi.aop.annotation.CacheEvictByBriefingId;
@@ -22,6 +26,25 @@ public class ScrapV2Api {
     @CacheEvictByBriefingId(value = "findBriefingsV2", briefingId = "#request.getBriefingId()")
     @Operation(summary = "05-01 Scrap📁 스크랩하기 V2", description = "브리핑을 스크랩하는 API입니다.")
     @PostMapping("/scraps/briefings")
+    @ApiResponses({
+            @ApiResponse(responseCode = "1000", description = "OK, 성공"),
+            @ApiResponse(
+                    responseCode = "AUTH003",
+                    description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH004",
+                    description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH006",
+                    description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "MEMBER_001",
+                    description = "사용자가 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
     public CommonResponse<ScrapResponse.CreateDTOV2> createV2(
             @RequestBody final ScrapRequest.CreateDTO request) {
         return CommonResponse.onSuccess(scrapFacade.create(request));
@@ -30,6 +53,25 @@ public class ScrapV2Api {
     @CacheEvictByBriefingId(value = "findBriefingsV2", briefingId = "#briefingId")
     @Operation(summary = "05-02 Scrap📁 스크랩 취소 V2", description = "스크랩을 취소하는 API입니다.")
     @DeleteMapping("/scraps/briefings/{briefingId}/members/{memberId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "1000", description = "OK, 성공"),
+            @ApiResponse(
+                    responseCode = "AUTH003",
+                    description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH004",
+                    description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH006",
+                    description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "MEMBER_001",
+                    description = "사용자가 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
     public CommonResponse<ScrapResponse.DeleteDTOV2> deleteV2(
             @PathVariable final Long briefingId, @PathVariable final Long memberId) {
         return CommonResponse.onSuccess(scrapFacade.delete(briefingId, memberId));
@@ -37,6 +79,25 @@ public class ScrapV2Api {
 
     @Operation(summary = "05-03 Scrap📁 내 스크랩 조회 V2", description = "내 스크랩을 조회하는 API입니다.")
     @GetMapping("/scraps/briefings/members/{memberId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "1000", description = "OK, 성공"),
+            @ApiResponse(
+                    responseCode = "AUTH003",
+                    description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH004",
+                    description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "AUTH006",
+                    description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(
+                    responseCode = "MEMBER_001",
+                    description = "사용자가 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+    })
     public CommonResponse<List<ScrapResponse.ReadDTOV2>> getScrapsByMemberV2(
             @PathVariable final Long memberId) {
         return CommonResponse.onSuccess(scrapFacade.getScrapsByMemberId(memberId));
