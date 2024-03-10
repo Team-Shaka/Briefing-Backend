20d4dea10b684d8a09a6dfea2a20080edb493554
swa07016 <swa07016@naver.com>
2024-03-10 20:49:54 +0900

    :sparkles: Feat: 애플로그인 랜덤 닉네임 세팅

---
+++ core/Briefing-Common/src/main/java/com/example/briefingcommon/common/constant/BriefingStatic.java
@@ -0,0 +1,7 @@
+package com.example.briefingcommon.common.constant;
+
+public class BriefingStatic {
+    public static final String NICK_NAME_FORMAT = "json";
+    public static final int NICK_NAME_COUNT = 1;
+    public static final int NICK_NAME_MAX_LENGTH = 8;
+}
