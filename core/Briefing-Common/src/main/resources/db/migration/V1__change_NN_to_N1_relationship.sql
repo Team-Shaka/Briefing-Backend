bf60b89334b33f14330151856d65d41efe462f0a
ahnsugyeong <ahnsugyeong@gmail.com>
2024-03-14 03:15:36 +0900

    :card_file_box: Feat: 스크립트 수정

--- core/Briefing-Common/src/main/resources/db/migration/V1__change_NN_to_N1_relationship.sql
+++ core/Briefing-Common/src/main/resources/db/migration/V1__change_NN_to_N1_relationship.sql
@@ -1,9 +1,3 @@
-ALTER TABLE article ADD COLUMN briefing_id BIGINT;
-
--- 기존 briefing_article 데이터를 기반으로 article 테이블의 briefing_id 업데이트
 UPDATE article a
 JOIN briefing_article ba ON a.id = ba.article_id
 SET a.briefing_id = ba.briefing_id;
-
--- briefing_article 테이블 제거
-DROP TABLE briefing_article;
