a1445ba0f15ab5088bee2d841a8926f23f87eb5e
ahnsugyeong <ahnsugyeong@gmail.com>
2024-08-10 01:44:26 +0900

    :bug: Fix: ErrorCode 오타 수정

--- core/Briefing-Common/src/main/java/com/example/briefingcommon/common/exception/common/ErrorCode.java
+++ core/Briefing-Common/src/main/java/com/example/briefingcommon/common/exception/common/ErrorCode.java
@@ -65,7 +65,7 @@ public enum ErrorCode {
     BAD_LAST_MESSAGE_ROLE(HttpStatus.BAD_REQUEST, "CHATTING_003", "마지막 메시지의 역할이 user가 아닙니다."),
     CAN_NOT_EMPTY_CONTENT(HttpStatus.BAD_REQUEST, "CHATTING_004", "content가 비어있습니다."),
     NOT_FOUND_ROLE(HttpStatus.BAD_REQUEST, "CHATTING_005", "해당하는 role을 찾을 수 없습니다."),
-    NOT_FOUND_MODEL(HttpStatus.BAD_REQUEST, "CHATTING_006", "해당하는 mode을 찾을 수 없습니다."),
+    NOT_FOUND_MODEL(HttpStatus.BAD_REQUEST, "CHATTING_006", "해당하는 model을 찾을 수 없습니다."),
 
     // subscription 에러
     SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "SUBSCRIPTION_001", "구독 정보가 존재하지 않습니다."),
